5445d8ea78e09a270676a46b38e2f8e3f755e007
Mulbruk <git@mulbr.uk>
2026-05-12 14:37:34 -0600

    Place benchmarking vs C behind a feature gate, so library can be used as a pure Rust dependency

--- build.rs
+++ build.rs
@@ -1,12 +1,15 @@
 fn main() {
-    println!("cargo:rerun-if-changed=build.rs");
-    println!("cargo:rerun-if-changed=cprobe/libsais_probe.c");
-    println!("cargo:rerun-if-changed=libsais/src/libsais.c");
-    println!("cargo:rerun-if-changed=libsais/include/libsais.h");
+    #[cfg(feature = "bench-vs-c")]
+    {
+        println!("cargo:rerun-if-changed=build.rs");
+        println!("cargo:rerun-if-changed=cprobe/libsais_probe.c");
+        println!("cargo:rerun-if-changed=libsais/src/libsais.c");
+        println!("cargo:rerun-if-changed=libsais/include/libsais.h");
 
-    cc::Build::new()
-        .file("cprobe/libsais_probe.c")
-        .include("libsais/include")
-        .flag_if_supported("-std=c99")
-        .compile("libsais_probe");
+        cc::Build::new()
+            .file("cprobe/libsais_probe.c")
+            .include("libsais/include")
+            .flag_if_supported("-std=c99")
+            .compile("libsais_probe");
+    }
 }
